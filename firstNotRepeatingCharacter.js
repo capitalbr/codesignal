f51a0131c163d82d0118abc523def67d0a65f2cf
Bradley <bbarnes.me@gmail.com>
2019-11-02 01:44:48 -0500

    Fix typo

--- firstNotRepeatingCharacter.js
+++ firstNotRepeatingCharacter.js
@@ -1,6 +1,6 @@
 //Given a string s consisting of small English letters, find 
 //and return the first instance of a non - repeating character 
-//in it.If there is no such character, return '_'.
+//in it. If there is no such character, return '_'.
 
 function firstNotRepeatingCharacter(s) {
   let alpha = {};
